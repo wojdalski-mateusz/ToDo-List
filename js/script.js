c5f217172fba5f824375c5eed514425c1a3bcc39
Mateusz Wojdalski <wojdalski.m@gmail.com>
2020-07-11 16:35:05 +0200

    refactor removeTask to Immutability

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@
     const addNewTask = (newTaskContent) => {
         tasks = [
             ...tasks, {
-            content: newTaskContent}];
+                content: newTaskContent
+            }];
         render();
     };
 
     const removeTask = (index) => {
-        tasks.splice(index, 1);
+        tasks = [
+            ...tasks.slice(0, index),
+            ...tasks.slice(index + 1)
+        ];
         render();
     };
 
